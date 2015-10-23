d2fc14c14a7885ef081e72b2f16e32a12536ea08
Hugo Baeta <hugo@baeta.me>
2015-10-22 23:37:55 -0700

    updates to footer and secondary

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
         <ul class="site-info">
             <li class="cms-info"><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'invenio' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'invenio' ), 'WordPress' ); ?></a></li>
-            <li class="theme-info"><a href="http://thefivethemes.com"><?php printf( __( 'Theme: %1$s by %2$s for %3$s', 'invenio' ), 'Invenio', 'Hugo Baeta', 'The Five Themes' ); ?></a></li>            
+            <li class="theme-info"><a href="http://thefivethemes.com"><?php printf( __( 'Theme: %1$s by %2$s for %3$s', 'invenio' ), 'Invenio', 'Hugo&nbsp;Baeta', 'The&nbsp;Five&nbsp;Themes' ); ?></a></li>            
         </ul><!-- .site-info -->
     </footer><!-- #colophon -->
 </div><!-- #page -->
